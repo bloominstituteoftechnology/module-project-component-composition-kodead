f7f63a1a54028a8ded960c44fd571e891587be44
Ahmad Koroma <ahmadkoroma25@outlook.com>
2023-12-14 05:39:53 -0500

    done2

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,8 +1,11 @@
 import React from "react"
 
 export default function Figure( {imageURL, caption}) {
+  return (
+
     <figure>
       <img src={imageURL} />
       <figcaption>Awsome pic taken on {caption}</figcaption>
     </figure>
+)
 }
